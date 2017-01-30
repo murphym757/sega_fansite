507efdf94d9854932dbfa4b629e0081cb4042458
murphym757 <murphym757@yahoo.com>
2017-01-30 17:28:07 -0500

    Running test on image display

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -55,7 +55,7 @@ class GamesController < ApplicationController
   def update
     respond_to do |format|
       if @game.update(game_params)
-        format.html { redirect_to games_url, notice: 'Game was successfully updated.' }
+        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
         format.json { render :show, status: :ok, location: @game }
       else
         format.html { render :edit }
