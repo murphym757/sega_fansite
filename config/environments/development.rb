d298810eaee8a8615b4dd64d0f7d8516b184a0e0
murphym757 <murphym757@yahoo.com>
2017-01-31 11:50:09 -0500

    Running a test on images via development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,16 +50,6 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Sets Paperclip to upload images to Amazon S3
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    }
-  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -71,6 +61,5 @@ Rails.application.configure do
   #Needed for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  #Needed for paperclip
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+
 end
