c92989053ec811859fc3597111cbc8af9c46abf5
murphym757 <murphym757@yahoo.com>
2017-01-31 17:54:56 -0500

    Removed search option

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  
+  resources :games do
+    collection do
+      get 'search'
+    end
       resources :reviews, except: [:show, :index]
   end
 
