f08195353228003e4652557f6fcabc1e714eb94b
murphym757 <murphym757@yahoo.com>
2017-02-09 00:15:30 -0500

    Added a delete button for removing a game on  the user's end

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -62,7 +62,7 @@ class GamesController < ApplicationController
   def destroy
     @game.destroy
     respond_to do |format|
-      format.html { redirect_to games_url, notice: 'Game was successfully destroyed.' }
+      format.html { redirect_to games_path, notice: 'Game was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
